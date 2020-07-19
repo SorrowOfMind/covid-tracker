0eaba4cc91c4a4c74b227a2b069938e7fe2eca44
Barbara Kumor <b.kumor@yahoo.com>
2020-07-19 18:36:23 +0200

    animation

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,12 @@ const apiCountries = "https://restcountries.eu/rest/v2/";
 const search = document.getElementById('search-bar');
 const lists = [...document.getElementsByClassName('country-list')];
 const x = document.getElementById('close-list');
+const listWrapper = document.getElementById('list');
+const countryName = document.getElementById('country-name');
 
 //global vars
 let countries = [];
+let query = '';
 
 window.addEventListener('load', () => {
     fetchCountries(apiCountries)
@@ -24,31 +27,43 @@ const fetchCountries = url => {
 const populateCountriesList = countriesArr => {
     console.log(countriesArr);
     countriesArr.forEach((country, idx) => {
-        let item = `<li class="list-item">${country.name}</li>`
+        let item = `<li class="list-item" id=${country.name}>${country.name}</li>`
         const countriesPerList = Math.ceil(countriesArr.length/3);
         let listIdx = Math.floor(idx/countriesPerList);
         lists[listIdx].insertAdjacentHTML('beforeend', item);
-    })
+    });
+    for (let list of lists) {
+        list.addEventListener('click', e => {
+            if (e.target && e.target.classList.contains('list-item')) {
+                query = e.target.textContent;
+                countryName.textContent = query;
+                listWrapper.classList.add('slide-up');
+                search.value = '';
+                console.log(query);
+            }
+        })
+    }
 }
 
-const makeList = (arr, ctr, i) => {
-        let item = `<li class="list-item">${ctr.name}</li>`
-        const countriesPerList = Math.ceil(arr.length/3);
-        let listIdx = Math.floor(i/countriesPerList);
-        lists[listIdx].insertAdjacentHTML('beforeend', item);
-}
+// const makeList = (arr, ctr, i) => {
+//         let item = `<li class="list-item">${ctr.name}</li>`
+//         const countriesPerList = Math.ceil(arr.length/3);
+//         let listIdx = Math.floor(i/countriesPerList);
+//         lists[listIdx].insertAdjacentHTML('beforeend', item);
+// }
 
 
 //show list on focus
 search.addEventListener('focus', () => {
-    list.classList.remove('hidden');
+    listWrapper.classList.remove('slide-up');
+    listWrapper.classList.add('slide-down');
 });
 
-//hide list on blur
-search.addEventListener('blur', () => {
-    list.classList.add('hidden');
-    search.value = '';
-});
+//close by x
+x.addEventListener('click', () => {
+    listWrapper.classList.remove('slide-down');
+    listWrapper.classList.add('slide-up');
+})
 
 //filter list on input
 search.addEventListener('input', e => {
@@ -60,4 +75,3 @@ search.addEventListener('input', e => {
         })
     })
 });
-
