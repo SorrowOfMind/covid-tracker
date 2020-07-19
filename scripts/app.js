dc7d67208b5a5e9b4745070ecc37845e774375ec
Barbara Kumor <b.kumor@yahoo.com>
2020-07-19 14:00:29 +0200

    filter countries on input

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
 const fetchCountries = url => {
     let countriesArr = [];
     axios.get(url)
-        .then(res => countries = res.data.reduce((acc, country) => acc.concat({country: country.name, code: country.alpha2Code}), []))
+        .then(res => countries = res.data.reduce((acc, country) => acc.concat({name: country.name, code: country.alpha2Code}), []))
         .then(() => populateCountriesList(countries))
         .catch(err => console.log(err));
 }
@@ -24,27 +24,40 @@ const fetchCountries = url => {
 const populateCountriesList = countriesArr => {
     console.log(countriesArr);
     countriesArr.forEach((country, idx) => {
-        let item = `<li>${country.country}</li>`
+        let item = `<li class="list-item">${country.name}</li>`
         const countriesPerList = Math.ceil(countriesArr.length/3);
-        let i = 0;
-        while (i < 3) {
-            if (i % countriesPerList === 0) {
-                lists[i].insertAdjacentHTML('beforeend', item);
-            }
-            i++;
-        }
+        let listIdx = Math.floor(idx/countriesPerList);
+        lists[listIdx].insertAdjacentHTML('beforeend', item);
     })
 }
+
+const makeList = (arr, ctr, i) => {
+        let item = `<li class="list-item">${ctr.name}</li>`
+        const countriesPerList = Math.ceil(arr.length/3);
+        let listIdx = Math.floor(i/countriesPerList);
+        lists[listIdx].insertAdjacentHTML('beforeend', item);
+}
+
 
 //show list on focus
 search.addEventListener('focus', () => {
     list.classList.remove('hidden');
 });
 
+//hide list on blur
 search.addEventListener('blur', () => {
     list.classList.add('hidden');
+    search.value = '';
 });
 
+//filter list on input
 search.addEventListener('input', e => {
-    console.log(e.target.value)
-})
+    const {value} = e.target;
+    lists.forEach(list => {
+        [...list.children].map(country => {
+            if (country.textContent.toLowerCase().indexOf(value.toLowerCase()) === -1) country.classList.add('hidden');
+            else country.classList.remove('hidden');
+        })
+    })
+});
+
